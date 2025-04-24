6fbbc246fd9befb63e4dfdc3d930ffb7c2e2a1d6
Kristófer Andersen <your.email@example.com>
2025-04-24 12:44:45 +0200

    2nd iteration TDD

---
+++ JSON-Parser-Tool/JSON.cs
@@ -0,0 +1,19 @@
+﻿namespace JSON_Parser_Tool;
+public class JSON
+{
+    public static object? Parse(string json)
+    {
+        object result = null;
+        for (int i = 0; i < json.Length; i++)
+        {
+            var c = json[i];
+            if (c == 'n')
+            {
+                result = null;
+                i += 4;
+                continue;
+            }
+        }
+        return result;
+    }
+}
--- JSONParser.Test/UnitTest1.cs
+++ JSONParser.Test/UnitTest1.cs
@@ -1,11 +1,21 @@
-﻿namespace JSONParser.Test
+﻿using JSON_Parser_Tool;
+
+namespace JSONParser.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ParseJson_Null_NullResult()
         {
+            var result = JSON.Parse("null");
+            Assert.Null(result);
+        }
 
+        [Fact]
+        public void ParseJson_Boolean_TrueResult()
+        {
+            var result = JSON.Parse("null");
+            Assert.Null(result);
         }
     }
 }
